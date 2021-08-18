d49a41632d87ce4ba9f63bbc683bf65fbd73e95a
Bryan <bryanesong@gmail.com>
2021-08-17 23:30:56 -0700

    master intial

---
+++ flask_main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from flask_mysqldb import MySQL
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['R_NAME'] = 'root'
+app.config['R_PLACE'] = 'root'
+app.config['FOOD_APP'] = 'FoodApp'
+
+mysql = MySQL(app)
+
+@app.route("/restaurants", methods=['GET', 'POST'])
+def resturant_route():
+    data = request.json
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run()
