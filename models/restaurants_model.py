f37631e582023dbad5a80567c45e61e1309761f5
Bryan <bryanesong@gmail.com>
2021-08-30 21:49:21 -0700

    Added restaurant route to flask main and added restaurant data model

---
+++ models/restaurants_model.py
@@ -0,0 +1,9 @@
+class Restaurant:
+
+    def __init__(self, price, hours, cusine, rating, popular_menu_items):
+        self.price = price
+        self.hours = hours
+        self.cusine = cusine
+        self.rating = rating
+        self.popular_menu_items = popular_menu_items
+    
