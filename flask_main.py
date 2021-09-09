64b8c2c20d1e1f6226dde8dca7d28c7dc4f880df
Bryan <bryanesong@gmail.com>
2021-09-09 16:33:25 -0700

    added flask sql connections

--- flask_main.py
+++ flask_main.py
@@ -1,22 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_mysqldb import MySQL
 from flask_marshmallow import Marshmallow
-from services.restaurants.restaurant_service import RestaurantService
-
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
 
-# this will only retrieve food places that are currently OPEN
-@app.route("/restaurants", methods=['GET', 'POST'])
-def resturant_route():
-    lat = request.args.get('lat')
-    long = request.args.get('long')
-    num_results = request.args.get('num_results')
-    # 47.5855941
-    # -122.0690723
+db = SQLAlchemy()
 
-    return RestaurantService().get_restaurant_in_area(lat=lat, long=long,num_results=num_results)
 
-#yeet
-if __name__ == '__main__':
-    app.run()
+def create_app():
+    """Construct the core application."""
+    app = Flask(__name__)
+
+    db.init_app(app)
+
+    with app.app_context():
+        import routes.routes  # import routes
+
+        db.create_all()  # Create database tables for our data models
+
+        return app
